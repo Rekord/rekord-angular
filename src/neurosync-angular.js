bee72f956402d0c0ba39779af31700592e207b04
Philip Diffenderfer <pdiffenderfer@gmail.com>
2015-12-02 12:45:26 -0500

    Resolves #4: Remove trailing slashes

--- src/neurosync-angular.js
+++ src/neurosync-angular.js
@@ -27,6 +27,11 @@
     Neuro.rest = function(database)
     {
 
+      function removeTrailingSlash(x)
+      {
+        return x.charAt(x.length - 1) === '/' ? x.substring(0, x.length - 1) : x;
+      }
+
       function execute( method, data, url, success, failure, offlineValue )
       {
         Neuro.debug( Neuro.Debugs.REST, this, method, url, data );
@@ -65,19 +70,19 @@
         },
         get: function( model, success, failure )
         {
-          execute( 'GET', undefined, database.api + model.$key(), success, failure );
+          execute( 'GET', undefined, removeTrailingSlash( database.api + model.$key() ), success, failure );
         },
         create: function( model, encoded, success, failure )
         {
-          execute( 'POST', encoded, database.api, success, failure, {} );
+          execute( 'POST', encoded, removeTrailingSlash( database.api ), success, failure, {} );
         },
         update: function( model, encoded, success, failure )
         {
-          execute( 'PUT', encoded, database.api + model.$key(), success, failure, {} );
+          execute( 'PUT', encoded, removeTrailingSlash( database.api + model.$key() ), success, failure, {} );
         },
         remove: function( model, success, failure )
         {
-          execute( 'DELETE', undefined, database.api + model.$key(), success, failure, {} );
+          execute( 'DELETE', undefined, removeTrailingSlash( database.api + model.$key() ), success, failure, {} );
         },
         query: function( query, success, failure )
         {
